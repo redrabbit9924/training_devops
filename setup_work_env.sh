880df60a89b2bb117a70647229511061094b1d32
cleansy bandit <cleanbandit5595@gmail.com>
2022-11-20 10:21:01 +0200

    add setup work env script

---
+++ setup_work_env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "-----------------------"
+echo "Setting work environment" 
+echo "-----------------------"
+
+echo "-------------"
+echo "Setting up git"
+echo "-------------"
+git config --global user.email "cleanbandit5595@gmail.com"
+git config --global user.name "cleansy bandit"
+
+echo "-------------"
+echo "Setting up projects dir"
+echo "-------------"
+
+mkdir -p ~/projects
